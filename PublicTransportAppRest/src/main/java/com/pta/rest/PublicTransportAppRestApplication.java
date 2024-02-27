cef91f29e4b048577535f0394646d5345ac3ab49
kfroemel <karoly.froemel@gmail.com>
2024-02-28 00:47:08 +0100

    rest module added

---
+++ PublicTransportAppRest/src/main/java/com/pta/rest/PublicTransportAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.pta.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicTransportAppRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicTransportAppRestApplication.class, args);
+    }
+
+}
---
+++ PublicTransportAppRest/src/test/java/com/pta/rest/PublicTransportAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pta.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicTransportAppRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
